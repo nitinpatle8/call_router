04dc68b575b7c6e487de20935c4cc535fccab43f
vaibhav-hash <techfashion0101@gmail.com>
2020-08-30 12:43:15 +0530

    message is here

--- call_router.cpp
+++ call_router.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <stack>
 
 using namespace std;
@@ -14,10 +15,6 @@ using namespace std;
 int base_no_counter = 0;
 
 ll *arr = (ll *)malloc(sizeof(ll) * MAX_BASE * MAX_MOBILE);
-
-// to store the address of base station address from base no
-Exchange **ex = (Exchange**) malloc(sizeof(Exchange**)*MAX_BASE);
-
 class Exchange
 { // Consider Exchange and base station same
 private:
@@ -57,6 +54,8 @@ public:
     }
 };
 
+// to store the address of base station address from base no
+Exchange **ex = (Exchange **)malloc(sizeof(Exchange **) * MAX_BASE);
 int Exchange::id_count = 0;
 
 void setMobileNos(Exchange *e)
@@ -91,7 +90,7 @@ Exchange *createTree()
 {
     Exchange *e = NULL;
     int choice;
-    cout << "Is it a base station 1 for YES, 0 for Exit:";
+    cout << "Is it a base station 1 for YES, 0 for Exchange area:";
     cin >> choice;
     if (choice)
     {
@@ -105,33 +104,35 @@ Exchange *createTree()
     {
         e = new Exchange();
         int treechoice;
-        cout << "Press 1 for left tree or 0 for exit:";
+        cout << "Press 1 for left tree or 0 for end process here:";
         cin >> treechoice;
         int start = 0;
         if (treechoice)
         {
             Exchange *eLeft = e->left = createTree();
             int i;
-            if(eLeft){
-                for(i = 0; i<MAX_BASE && eLeft->set[i]!=-1; i++){
+            if (eLeft)
+            {
+                for (i = 0; i < MAX_BASE && eLeft->set[i] != -1; i++)
+                {
                     e->set[i] = eLeft->set[i];
                 }
                 start = i;
             }
-            
         }
         cout << "Press 1 for right tree or 0 for exit:";
         cin >> treechoice;
         if (treechoice)
         {
             Exchange *eRight = e->right = createTree();
-            if(eRight){
+            if (eRight)
+            {
                 int i;
-                for(i = 0; i<MAX_BASE && eRight->set[i]!=-1; i++){
-                    e->set[i+start] = eRight->set[i];
+                for (i = 0; i < MAX_BASE && eRight->set[i] != -1; i++)
+                {
+                    e->set[i + start] = eRight->set[i];
                 }
             }
-        
         }
     }
     return e;
@@ -165,7 +166,7 @@ bool isMember(Exchange *e, ll m)
         {
             int t = h(e->set[i], m), copy_value = t;
             int index = e->set[i] % MAX_BASE * 100 + MAX_MOBILE;
-            while (arr[t] != 0 || arr[t]!=-1)
+            while (arr[t] != 0 || arr[t] != -1)
             {
                 if (arr[t] == m)
                 {
@@ -195,7 +196,7 @@ bool insertMobile(Exchange *e, ll mobileNo)
 {
     int t = h(e->base_no, mobileNo);
     int index = e->base_no % MAX_BASE * 100 + MAX_MOBILE; // LAST LOCATION OF BASE STATION INDEX
-    while (arr[t] != 0 || arr[t]!=-1)
+    while (arr[t] != 0 || arr[t] != -1)
     {
         t += 1;
         if (t == index)
@@ -204,6 +205,7 @@ bool insertMobile(Exchange *e, ll mobileNo)
         }
     }
     arr[t] = mobileNo;
+    return true;
 }
 
 // Delete (MobilePhone m): Deletes m from the set/tree, return false if m is not in the set.
@@ -227,7 +229,7 @@ bool deleteMobile(Exchange *e, ll mobileNo)
             cout << "There is no such number present:" << mobileNo << endl;
             break; //this indicates whole base stations member has checked and now the loop is starting to run again
         }
-       else if (t == index)
+        else if (t == index)
         {
             t = e->base_no % MAX_BASE * 100;
         }
@@ -280,35 +282,44 @@ int findPhone(Exchange *e, ll m)
 }
 
 // Exchange lowestRouter (base a, base b) this function finds out the common ancestor in the tree
-Exchange* lowestRouter(Exchange* root, Exchange *a, Exchange *b){
+Exchange *lowestRouter(Exchange *root, Exchange *a, Exchange *b)
+{
     Exchange *rootLeft = lowestRouter(root->left, a, b);
     Exchange *rootRight = lowestRouter(root->right, a, b);
 
-    if(root == a) return a;
-    if(root == b) return b;
-    if(rootLeft == a && rootRight == b) return root;
-    if(rootLeft) return rootLeft;
-    if(rootRight) return rootRight;
+    if (root == a)
+        return a;
+    if (root == b)
+        return b;
+    if (rootLeft == a && rootRight == b)
+        return root;
+    if (rootLeft)
+        return rootLeft;
+    if (rootRight)
+        return rootRight;
     return NULL;
-
 }
 
-
 // stores path from common anscestor to the exchange b in stack v
-bool pathFromCommonAnscestor(Exchange *lcr, Exchange *b, stack<Exchange*> &v){
-    if(lcr){
+bool pathFromCommonAnscestor(Exchange *lcr, Exchange *b, stack<Exchange *> &v)
+{
+    if (lcr)
+    {
         v.push(lcr);
-        if(lcr == b){
+        if (lcr == b)
+        {
             return true;
         }
         bool eLeft = pathFromCommonAnscestor(lcr->left, b, v);
-    
-        if(eLeft){
+
+        if (eLeft)
+        {
             return true;
         }
 
         bool eRight = pathFromCommonAnscestor(lcr->right, b, v);
-        if(eRight){
+        if (eRight)
+        {
             return true;
         }
         v.pop();
@@ -316,36 +327,40 @@ bool pathFromCommonAnscestor(Exchange *lcr, Exchange *b, stack<Exchange*> &v){
     return false;
 }
 // commonPath finds the common path between two base stations
-// given a , b are 2 base stations 
-stack<Exchange*> &commonPath(Exchange *root, Exchange *a, Exchange *b){
+// given a , b are 2 base stations
+stack<Exchange *> &commonPath(Exchange *root, Exchange *a, Exchange *b)
+{
 
     Exchange *lcr = lowestRouter(root, a, b);
-    stack<Exchange*> *s1 = new stack<Exchange*>;
+    stack<Exchange *> *s1 = new stack<Exchange *>;
     pathFromCommonAnscestor(lcr, a, *s1);
-    stack<Exchange*> *s2 = new stack<Exchange*>;
+    stack<Exchange *> *s2 = new stack<Exchange *>;
     pathFromCommonAnscestor(lcr, b, *s2);
 
     Exchange *temp = NULL;
-    while(s1->top() == s2->top()){
+    while (s1->top() == s2->top())
+    {
         temp = s1->top();
         s1->pop();
         s2->pop();
     }
-    if(temp) s1->push(temp);
+    if (temp)
+        s1->push(temp);
     return *s1;
 }
 
 // ExchangeList routeCall(MobilePhone a, MobilePhone b) finding shorted path from one node to another.
 
-stack<Exchange*> *routeCall(Exchange *root, ll a, ll b){
-
+stack<Exchange *> *routeCall(Exchange *root, ll a, ll b)
+{
 
     int baseA = findPhone(root, a);
     int baseB = findPhone(root, b);
-    if(baseA>0 && baseB>0){
-        Exchange *baseExA =  ex[baseA];
+    if (baseA > 0 && baseB > 0)
+    {
+        Exchange *baseExA = ex[baseA];
         Exchange *baseExB = ex[baseB];
-        stack<Exchange*> *s = &commonPath(root, baseExA, baseExB);
+        stack<Exchange *> *s = &commonPath(root, baseExA, baseExB);
         return s;
     }
     return NULL;
@@ -356,30 +371,31 @@ stack<Exchange*> *routeCall(Exchange *root, ll a, ll b){
 // other data (sets of mobiles maintained at different levels) that is maintained could also change.
 // delete mobile no m from its current exchange to exchange e
 // simple one
-bool movePhone(ll m, Exchange *e, Exchange *root){
+bool movePhone(ll m, Exchange *e, Exchange *root)
+{
 
     int baseM = findPhone(root, m);
     Exchange *baseStation = ex[baseM];
     bool flag1 = deleteMobile(baseStation, m);
     bool flag2 = true;
-    if(flag1){
+    if (flag1)
+    {
         flag2 = insertMobile(e, m);
     }
-    if(flag1 && flag2)
+    if (flag1 && flag2)
         return true;
-    return false; 
+    return false;
 }
-
 
 // print the stack
-void printStack(stack<Exchange*> &s){
- while (!s.empty()) { 
-        cout<<"The Id_Number Is:"<<s.top()->id_no<<endl; 
-        s.pop(); 
+void printStack(stack<Exchange *> &s)
+{
+    while (!s.empty())
+    {
+        cout << "The Id_Number Is:" << s.top()->id_no << endl;
+        s.pop();
     }
-
 }
-
 
 int main()
 {
@@ -389,93 +405,156 @@ int main()
     cout << "******First you have to create the tree" << endl;
 
     Exchange *e = createTree();
+    ll mobile_no;
+    int base_no;
+    Exchange *ea, *eb;
+    bool ans;
+    while (1)
+    {
+        int exit_loop;
+        cout << "If you want to terminate the process here Enter 0 if not enter any integer:" << endl;
+        cin>>exit_loop;
+        if (exit_loop == 0)
+        {
+            break;
+        }
+        else
+        {
+            int choice;
 
-    int choice;
+            cout << "Select the options given below" << endl;
 
-    cout << "Select the options given below" << endl;
+            cout << "1. Has anyone registered to our mobile Services:" << endl;
+            cout << "2. Is your mobile number listed to our telecom Services:" << endl;
+            // insert mobile number
+            cout << "3. Register your mobile number by base station base no i.e. location:" << endl;
+            // delete mobile number
+            cout << "4. Deregister your mobile number:" << endl;
+            cout << "5. Find where your mobile number location is:" << endl;
+            cout << "6. for finding lowest common ancwestor:" << endl;
+            cout << "7. For finding path between two base station" << endl;
+            cout << "8. Move your mobile number to Base Station A to Base Station B:" << endl;
 
-    cout << "1. Has anyone registered to our mobile Services:" << endl;
-    cout << "2. Is your mobile number listed to our telecom Services:" << endl;
-    // insert mobile number
-    cout << "3. Register your mobile number by base station base no i.e. location:" << endl;
-    // delete mobile number
-    cout << "4. Deregister your mobile number:" << endl;
-    cout << "5. Find where your mobile number location is:"<< endl;
-    cout << "6. Moving your mobile number to another:" << endl;
-    cout << "7. Move your mobile number to Base Station A to Base Station B:" << endl;
-
-    cin >> choice;
-    switch(choice){
-        case 1:
+            cin >> choice;
+            /* code */
+            switch (choice)
+            {
+            case 1:
                 // e is the root exchange
-                bool ans=isEmpty(e);
-                if(ans==true)
+                ans = isEmpty(e);
+                if (ans == true)
                 {
-                    cout<<"Yes We have lot of customers for privacy we can't show their numbers;"<<endl;
+                    cout << "Yes We have lot of customers for privacy we can't show their numbers;" << endl;
                 }
                 else
                 {
-                    cout<<"Sorry We don't have customers Bt in future we will have most customers in World:"<<endl;
+                    cout << "Sorry We don't have customers Bt in future we will have most customers in World:" << endl;
                 }
                 break;
-        case 2:
-                ll mobile_no;
-                cin>>mobile_no;
-                bool ans=isMember(e,mobile_no);
-                if(ans==true)
-                {
-                    cout<<"	We are happy to say that You are our friendly customer:"<<endl;
-                }
-                else
-                {
-                    cout<<"Sorry to say that bt You are not our customer so join us for your benefit:"<<endl;
-                }
-                break;
-        case 3:
-                int base_no;
-                cin >> base_no;
-                ll mobile_no;
+            case 2:
                 cin >> mobile_no;
-                Exchange *bs = ex[base_no];
-                insertMobile(bs, mobile_no);
-                break;
-        case 4:
-                long long int mobile_no;
-                cin>>mobile_no;
-                bool ans=deleteMobile(e,mobile_no);
-                if(ans==true)
+                ans = isMember(e, mobile_no);
+                if (ans == true)
                 {
-                    cout<<"	Sorry to let you go:"<<endl;
+                    cout << "	We are happy to say that You are our friendly customer:" << endl;
                 }
                 else
                 {
-                    cout<<"You already deregistered yourself:"<<endl;
+                    cout << "Sorry to say that bt You are not our customer so join us for your benefit:" << endl;
                 }
                 break;
-        case 5:
-                long long int mobile_no;
-                int ans=findPhone(e,mobile_no);
-                if(ans==-1)
+            case 3:
+                cout << "Enter The base _no:";
+                cin >> base_no;
+                cout << "Enter the mobile number:";
+                // ll mobile_no;
+                cin >> mobile_no;
+                Exchange *bs;
+                bs = ex[base_no];
+                ans = insertMobile(bs, mobile_no);
+                if (ans == 1)
                 {
-                    cout<<"	Sorry to say that you are not Registered On Network:"<<endl;
+                    cout << "You are now our Customer successfully registered:" << endl;
                 }
                 else
                 {
-                    cout<<"The Bast station You are connected Is:"<<ans<<endl;
+                    cout << "Something went wrong not registered:" << endl;
                 }
                 break;
-        case 6:
-                
+            case 4:
+                // long long int mobile_no;
+                cout << "Plzz enter your number to end your subscription:";
+                cin >> mobile_no;
+                ans = deleteMobile(e, mobile_no);
+                if (ans == true)
+                {
+                    cout << "	Sorry to let you go:" << endl;
+                }
+                else
+                {
+                    cout << "You already deregistered yourself:" << endl;
+                }
                 break;
-        case 7:
-        
+            case 5:
+                cout << "Enter your number for your present base station:";
+                // long long int mobile_no;
+                cin >> mobile_no;
+                int ans1;
+                ans1 = findPhone(e, mobile_no);
+                if (ans1 == -1)
+                {
+                    cout << "	Sorry to say that you are not Registered On Network:" << endl;
+                }
+                else
+                {
+                    cout << "The Bast station You are connected Is:" << ans1 << endl;
+                }
                 break;
-        default:
-            cout<<"Give an right Input:"<<endl;
-        break;
-    
+            case 6:
+                cout << "For Lowest common possible router:" << endl;
+                cout << "Enter Base station Numbers:";
+                int a, b;
+                cin >> a;
+                cin >> b;
+                ea = ex[a];
+                eb = ex[b];
+
+                Exchange *ans2;
+                ans2 = lowestRouter(e, ea, eb);
+                cout << "The Lowest Router:(" << ans2->id_no << ")" << endl;
+                break;
+            case 7:
+                cout << "For path Between baseStations:" << endl;
+                cout << "Enter Base station Numbers:";
+                // int a, b;
+                cin >> a;
+                cin >> b;
+                ea = ex[a];
+                eb = ex[b];
+                printStack(commonPath(e, ea, eb));
+                break;
+            case 8:
+                cout << "	For  changing base station:" << endl;
+                cout << "	Enter current base station number:";
+                cin >> base_no;
+                // long long int mb;
+                cout << "Enter The Mobile number:" << endl;
+                eb = ex[base_no];
+                ans = movePhone(mobile_no, eb, e);
+                if (ans == true)
+                {
+                    cout << "Successfully transfered:" << endl;
+                }
+                else
+                {
+                    cout << "Sorry Bt Not transfered:" << endl;
+                }
+                break;
+            default:
+                cout << "Give an right Input:" << endl;
+                break;
+            }
+        }
     }
-
-
     return 0;
 }
